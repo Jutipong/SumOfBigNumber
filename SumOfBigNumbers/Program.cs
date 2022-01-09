744c085604dfe03e2a99fe6d7fef33281092abe9
macbook-air <techcom.ec20@gmail.com>
2022-01-09 22:37:46 +0700

    Add: Logic in Program.cs

--- SumOfBigNumbers/Program.cs
+++ SumOfBigNumbers/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using SumOfBigNumbers.Services;
+using System;
 
 namespace SumOfBigNumbers
 {
     class Program
     {
+        private static readonly SumOfNumberService _SumOfNumberService = new SumOfNumberService();
         static void Main(string[] args)
         {
-            Console.Write("OK");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Press ESC to exit..\n");
+
+                var input1 = _SumOfNumberService.GetInputNumber("A");
+                var input2 = _SumOfNumberService.GetInputNumber("B");
+                var result = _SumOfNumberService.SumBigInteger(input1, input2);
+
+                Console.WriteLine($"A + B = {result}\n");
+                Console.Write("Press Any key to continue... ");
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
