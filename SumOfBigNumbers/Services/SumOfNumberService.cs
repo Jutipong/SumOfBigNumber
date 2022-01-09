13ff6f9774f804d2e7285ef3374345e1779b7c40
macbook-air <techcom.ec20@gmail.com>
2022-01-09 20:50:03 +0700

    Add: Function => SumBigInteger()

---
+++ SumOfBigNumbers/Services/SumOfNumberService.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace SumOfBigNumbers.Services
+{
+    public class SumOfNumberService
+    {
+        public BigInteger SumBigInteger(BigInteger inputA, BigInteger inputB)
+        {
+            return inputA + inputB;
+        }
+    }
+}
